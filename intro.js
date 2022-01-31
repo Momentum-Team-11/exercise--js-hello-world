8087fdef1f699b0adf989a54373fdafb84d6ee59
Amy Gori <amydgori@gmail.com>
2022-01-30 21:17:01 -0500

    Update exercise for clarity

--- intro.js
+++ intro.js
@@ -1,45 +1,96 @@
 // Intro JS exercise
-console.log('oh hai! Welcome to the console!')
-console.log('You can run JavaScript directly here.')
-console.log('You can also log any output from your JavaScript files.')
+console.log('oh hai! 👋 Welcome to the console!')
+console.log(
+  'This is where you can see output that you have printed out (_logged_) from your JavaScript files using console.log().'
+)
+console.log(
+  'You can also run JavaScript directly here, just start typing. When you press return or enter, the JavaScript you typed will be evaluated.'
+)
 
 // Read carefully and uncomment the necessary lines one at a time as you run the code.
 
-// 1. What does `firstName` evaluate to on this line? Console.log to find out.
-let firstName = 'Jamal'
-// Write your console.log statement here
+/*
+ASSIGN A VALUE TO A VARIABLE AND LOG IT TO THE CONSOLE
 
-// 2. Below, a new value is assigned to the firstName variable.
-// What does `firstName` evaluate to after reassignment?
-// Uncomment the following lines and see what it is with console.log:
+Read the code below.
+*/
+let firstName = 'Jamal'
+console.log("Here's some output from our first line of JavaScript in intro.js")
+console.log(
+  "On the next line 👇 we'll log the value of the firstName variable."
+)
+console.log(firstName)
+
+/*
+YOUR TURN.
+😺 Write a value for the variable defined on line 28. It's commented out so that it won't cause an error.
+Once you have written the value, remove the comment characters, save this file, and reload the page in the browser.
+/*
+// const myName =
+// console.log(myName)
+
+
+/*
+ASSIGN A NEW VALUE TO A PREVIOUSLY DEFINED VARIABLE
+
+Below, a new value is assigned to the firstName variable that we defined on line 17 above.
+Read the code. What do you expect that the `firstName` variable will evaluate to after reassignment?
+
+😺 Uncomment lines 41 and 42, save this file, reload the browser page, and check the output in the console:
+*/
 // firstName = "Avery"
 // console.log("The firstName variable now has an assigned value of ", firstName)
 
-// 3. What is happening here? Use console.log() to check if you're right.
-// TODO: Uncomment the following lines:
+/*
+CREATE A NEW VARIABLE USING THE VALUE FROM ANOTHER VARIABLE
+
+What is happening here? Use console.log() to check if you're right.
+
+😺 Uncomment the following lines:
+*/
 // let name = firstName
 // console.log("The name variable evaluates to ", name)
 
-// 4. Assign a value to the `greeting` variable that is a string that represents a greeting, and log it to the console.
-let greeting
+/*
+😺 Create a variable named `greeting`.
+Assign it a value of a string like "Hi" or "Hey there" (or anything you like!).
+Log the `greeting` variable's value to the console.
+*/
+// Put your code here!
 
-// 5. Combine the `greeting` and `name` variables to output a personalized greeting. Log it to the console.
+/* COMBINE STRINGS USING VARIABLES
+
+😺 Uncomment line 65 to see how you can combine strings with variables to create a dynamic sentence!
+/*
+console.log("Nice to meet you, " + firstName + "! My name is " + myName + ".")
+
+😺 Combine the `greeting` and `name` variables to output a personalized greeting like "Hi, Avery!".
+Log it to the console.
+*/
 // Write your code here
 
-// 6. What does `result` evaluate to?
-// Uncomment the following lines and use console.log() to check what value is stored in the variable `result`
+/*
+USING VARIABLES WITH MATH
+
+What do you expect that the variable `result` will evaluate to?
+😺 Uncomment the following lines and use console.log() to check what value is stored in the variable `result`
+*/
 // let num1 = 50
 // let num2  = 10
 // let result = num1 - num2
 
-// 7. What will `result` return here? Read this code carefully.
-// Log it to the console to find out what happens.
-// Uncomment the following line:
+/* What will `result` return here? Read this code carefully.
+😺 Uncomment line 85, and write a console.log to see what `result` will evaulate to.
+*/
 // result = num1 - "5"
+// replace this line with a console.log() that shows what `result` is.
 
-// 8. Create a variable to calculate the total number of tacos and burritos.
-// Log to the console a sentence that tells the user the total number, for instance:
-// "The total number of tacos and burritos is 12"
-// Uncomment the following lines and then write your code:
+/*
+😺 Create a variable to calculate the total number of tacos and burritos.
+Log to the console a sentence that tells the user the total number, for instance:
+"The total number of tacos and burritos is 12"
+Uncomment the following lines and then write your code.
+*/
 // let tacos = 8
 // let burritos = 4
+// Write your code here!
